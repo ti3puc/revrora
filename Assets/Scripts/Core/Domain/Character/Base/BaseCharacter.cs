f3b44d3313d21e6da3d85162cc1274a1c69b9649
Lucas Coutinho <lucasctnh@hotmail.com>
2024-09-26 19:38:01 -0300

    feat: Add scriptable for base character stats

--- Assets/Scripts/Core/Domain/Character/Base/BaseCharacter.cs
+++ Assets/Scripts/Core/Domain/Character/Base/BaseCharacter.cs
@@ -4,8 +4,7 @@ namespace Character.Base
 {
     public class BaseCharacter : MonoBehaviour
     {
-
+        [SerializeField] private BaseCharacterStats _baseCharacterStats;
         private CharacterStats _characterStats;
-        
     }
 }
